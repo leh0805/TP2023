e9357a8c1aa61d54a111951fba225c86c4e99c4d
leh0805 <ferreira.leticia@gsuite.iff.edu.br>
2023-10-11 14:36:29 -0300

    rtytf7g7

--- servidor.js
+++ servidor.js
@@ -1,45 +1,55 @@
-const express = require("express")
-const fs= require("fs")
+const express = require('express')
 const app = express()
-app.use(express.urlencoded({extended:true}))
+const mongoose = require('mongoose')
+app.use(express.json())
 
 
-app.set('view engine', 'ejs')
-     
-app.post('/home', function(req,res){
-  const salvar = JSON.stringify(req.body)
-  fs.writeFileSync(`${req.body.email}.json`, salvar, (err) =>{
-   if(err){
-        res.send({message: err})
-   }else{
-         res.send({message: 'ok'})  
-   }
-  } )
+
+const dotenv = require('dotenv')
+
+if(process.env.devouprod === "dev"){
+dotenv.config({path:'./config/.env.dev'})
+}
+if (process.env.devouprod === "prod"){
+   dotenv.config({path: "./config/.env.prod"})
+}
+
+
+
+
+const modelodeUsuario = mongoose.model('contas', new mongoose.Schema({
+    email: String,
+    password: String
+}))
+
+mongoose.connect(process.env.bancoDdados)
+.then(function(){
+
+app.get('/get/:email', async (req,res)=>{
+    const usuarioEncontrado = await modelodeUsuario.findOne({email: req.params.email})
+    console.log(usuarioEncontrado);
+    res.send(usuarioEncontrado)
+})
  
-})
-app.get('/lista', function(req, res) {
-   fs.readdir(__dirname, (err,files)=>{
-     var dados = files.filter(file => file.includes('@') && file.includes('.json'))
-     res.send( {dados: dados})
-   
-   })
- })
-app.get('/dados/:email', (req,res)=>{
-   var dados = req.params
-   var dadosfinais = JSON.parse(fs.readFileSync(dados.email))
-   res.send({dados: dadosfinais})
-})
-app.get('/apagar/:email', (req,res)=>{
-   var dados = req.params
-   if (fs.existsSync(dados.email)){
-        res.send({message: 'ok'})
-       fs.rmSync(dados.email)
-   }else{
-        res.send({message: 'erro'})
-   }
+app.post('/post',async (req,res) =>{
+    const usuarioCriado = await modelodeUsuario.create({email: req.body.email, password: req.body.password})
+    res.send(usuarioCriado)
 })
 
+app.put('/put', async (req,res)=>{
+    const usuarioAtualizado = await modelodeUsuario.findOneAndUpdate({email: req.body.email, password: req.body.password}, {email: req.body.newemail, password: req.body.newpassword})
+    res.send(usuarioAtualizado)
+})
+  
+app.delete('/delete', async (req,res)=>{
+    const usuarioDeletado = await modelodeUsuario.deleteOne({email: req.body.email, password: req.body.password})
+    res.send(usuarioDeletado)
+})  
 
-app.listen(8080, () =>{
-   console.log("programa funcionando")
+app.use((req,res)=>{
+    res.send('Não foi possível encontrar sua rota')
+})
+
+app.listen(3000, ()=>console.log(`O servidor ta rodando nessa porta aí meu fiel ${3000}`))
+
 })
