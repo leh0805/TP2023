6da3fa1758fe5ac2510c553c7391c110730e0271
leh0805 <ferreira.leticia@gsuite.iff.edu.br>
2023-11-30 09:40:59 -0300

    Atualização

--- ativMong.js
+++ ativMong.js
@@ -37,6 +37,18 @@ app.post('/post',async (req,res) =>{
     res.send(usuarioCriado)
 })
 
+
+app.post('/post',async (req,res) =>{
+    const usuarioCriado = await modelodeUsuario.create({comment:req.body.comment})
+    res.send({ message: "Crie uma nova resenha aqui:" })
+})
+
+
+app.post('/post',async (req,res) =>{
+    const usuarioCriado = await modelodeUsuario.findOne({comment:req.body.comment})
+    res.send({ message: "Procure outras resenhas aqui:" })
+})
+
 app.put('/put', async (req,res)=>{
     const usuarioAtualizado = await modelodeUsuario.findOneAndUpdate({email: req.body.email, password: req.body.password}, {email: req.body.newemail, password: req.body.newpassword})
     res.send({ message: "Dados atualizados com sucesso!" })
@@ -51,6 +63,6 @@ app.use((req,res)=>{
     res.send('Não foi possível encontrar sua rota')
 })
 
-app.listen(2800, ()=>console.log(`O servidor ta rodando nessa porta aí meu fiel: ${2800}`))
+app.listen(2800, ()=>console.log(`O servidor esta rodando nessa porta: ${2800}`))
 
 })
